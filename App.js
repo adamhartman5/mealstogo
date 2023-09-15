0ddbabfec1e4d432f920b97c292688422599842c
Adam Hartman <adamhartman5@gmail.com>
2023-09-15 01:29:41 -0400

    Set up restaurant card and style

--- App.js
+++ App.js
@@ -1,31 +1,11 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
 	return (
 		<>
-			<SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-				<View style={styles.search}>
-					<Text>search</Text>
-				</View>
-				<View style={styles.list}>
-					<Text>list</Text>
-				</View>
-			</SafeAreaView>
+			<RestaurantsScreen />
 			<ExpoStatusBar style='auto' />
 		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	search: {
-		padding: 16,
-		backgroundColor: 'green',
-	},
-	list: {
-		padding: 16,
-		flexGrow: 1,
-		backgroundColor: 'blue',
-	},
-});
