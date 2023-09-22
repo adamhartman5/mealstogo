ee3472595055148ff54d10b206a18b66329421ae
Adam Hartman <adamhartman5@gmail.com>
2023-09-21 23:58:40 -0400

    Add spacer and install react-navigation

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,6 +4,7 @@ import { Searchbar } from 'react-native-paper';
 import { StatusBar, FlatList, SafeAreaView } from 'react-native';
 
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const SafeArea = styled(SafeAreaView)`
 	flex: 1;
@@ -16,9 +17,9 @@ const SearchContainer = styled.View`
 
 const RestaurantList = styled(FlatList).attrs({
 	contentContainerStyle: {
-		padding: 16
-	}
-})
+		padding: 16,
+	},
+})``;
 
 export const RestaurantsScreen = () => (
 	<SafeArea>
@@ -26,8 +27,25 @@ export const RestaurantsScreen = () => (
 			<Searchbar />
 		</SearchContainer>
 		<RestaurantList
-			data={[{name: 1}, {name: 2},{name: 3}, {name: 4},{name: 5}, {name: 6},{name: 7}, {name: 8},{name: 9}, {name: 10},{name: 11}, {name: 12},]}
-			renderItem={() => <RestaurantInfoCard />}
+			data={[
+				{ name: 1 },
+				{ name: 2 },
+				{ name: 3 },
+				{ name: 4 },
+				{ name: 5 },
+				{ name: 6 },
+				{ name: 7 },
+				{ name: 8 },
+				{ name: 9 },
+				{ name: 10 },
+				{ name: 11 },
+				{ name: 12 },
+			]}
+			renderItem={() => (
+				<Spacer position='bottom' size='large'>
+					<RestaurantInfoCard />
+				</Spacer>
+			)}
 			keyExtractor={(item) => item.name}
 		/>
 	</SafeArea>
